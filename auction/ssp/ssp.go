425fc5de90bfcaf38c085aaa459fb33916a3df73
demonoid81 <daemon84@mail.ru>
2022-01-17 00:07:43 +0300

    add feed_metrics

--- auction/ssp/ssp.go
+++ auction/ssp/ssp.go
@@ -8,6 +8,7 @@ import (
 	"github.com/demonoid81/dsp/config"
 	"github.com/demonoid81/dsp/events/encrypt"
 	"github.com/demonoid81/dsp/events/utils"
+	"github.com/prometheus/client_golang/prometheus"
 	"go.mongodb.org/mongo-driver/mongo"
 	"math/rand"
 	"net/http"
@@ -15,7 +16,7 @@ import (
 	"sync"
 )
 
-func Feed(ctx context.Context, SSPData []dsp.SSP, waitGroup *sync.WaitGroup, mongoClient *mongo.Client) http.HandlerFunc {
+func Feed(ctx context.Context, SSPData []dsp.SSP, waitGroup *sync.WaitGroup, mongoClient *mongo.Client, counter *prometheus.CounterVec) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 
@@ -56,8 +57,6 @@ func Feed(ctx context.Context, SSPData []dsp.SSP, waitGroup *sync.WaitGroup, mon
 			w.Header().Set("Token", dataBase64)
 
 			result := map[string]interface{}{}
-
-			fmt.Println("ssp_id", creative.SSPID)
 
 			linkData := map[string]interface{}{
 				"link":     creative.Link,
@@ -117,6 +116,8 @@ func Feed(ctx context.Context, SSPData []dsp.SSP, waitGroup *sync.WaitGroup, mon
 				}
 			}
 
+			counter.WithLabelValues("feed", fmt.Sprintf("$d", creative.SSPID)).Inc()
+
 			res, err := json.Marshal(result)
 			if err != nil {
 				w.WriteHeader(http.StatusNoContent)
@@ -130,312 +131,3 @@ func Feed(ctx context.Context, SSPData []dsp.SSP, waitGroup *sync.WaitGroup, mon
 		w.WriteHeader(http.StatusNoContent)
 	}
 }
-
-//var SSPData = []dsp.SSP{
-//	{
-//		Key:  "YxXs1HSVgDRRC9T",
-//		Name: "Mgid",
-//		ID:   102,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                102,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "xBXhzjjhhYDFU7r",
-//		Name: "test",
-//		ID:   101,
-//		DSP: []dsp.DSP{{
-//			ID:                102,
-//			Profit:            0.00,
-//			SourceIdBlacklist: []string{"102101back_block_lp_2087", "102101ramos_w10_1304_us_edge_new", "102101lux_w10_0303-dagger-2033", "102101ramos_w10_0605_us_edge", "102101terame_w10_1609_edge_486231", "102101ramos_w10_1608_topiced", "102101ramos_w10_0907_us_edge", "102101ceca_w10_1608_kasa", "102101yue_w10_1208_manunited"},
-//			CountryBlacklist:  []string{},
-//			CountryWhitelist:  []string{},
-//		}},
-//	},
-//	{
-//		Key:  "aeW4bHWkaGIMl6N",
-//		Name: "Clickadilla_In-Page",
-//		ID:   111,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                103,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "qjHLq1RZXU3tk8j",
-//		Name: "Clickadilla_In-Page",
-//		ID:   112,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                103,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "wKyozxnWWgdHECi",
-//		Name: "Clickadilla_In-Page",
-//		ID:   113,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                103,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "STx9xtDhW0825P8",
-//		Name: "Clickadilla_In-Page",
-//		ID:   114,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                103,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "lz5ouNvlSnlZOIf",
-//		Name: "DaoPush",
-//		ID:   115,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                102,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "UvEH4RJTsa2oOpx",
-//		Name: "DaoPush_Classic",
-//		ID:   116,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                102,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "NaENhvugH5WdQ1y",
-//		Name: "DaoPush_In-Page",
-//		ID:   117,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                103,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "ngGnozEJ3syxUEp",
-//		Name: "DaoPush_In-Page",
-//		ID:   118,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                103,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "wQdvtTF2b6fs2p2",
-//		Name: "RexRTB",
-//		ID:   103,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                102,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "PJtnhleyzNYAdGk",
-//		Name: "RiverTraffic_Classic",
-//		ID:   104,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                102,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "z8GBO0tCQwTw2bt",
-//		Name: "RiverTraffic_In-Page",
-//		ID:   105,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                103,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "wlFmvmEl5spoPxk",
-//		Name: "RiverTraffic_In-Page",
-//		ID:   106,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                103,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "FqYgmeGAJG9nJz6",
-//		Name: "Clickadilla_In-Page",
-//		ID:   109,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                103,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "vv8VZtulWS5KvvC",
-//		Name: "Clickadilla_In-Page",
-//		ID:   110,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                103,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "s9u1H1aJuJgYWJc",
-//		Name: "Admaven_Adult_push",
-//		ID:   119,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                102,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "O29B7nMFGE6fFNv",
-//		Name: "Admaven_MNSTRM_push",
-//		ID:   120,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                102,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "uiUIGofvdGxzmA4",
-//		Name: "Clickadilla_Classic",
-//		ID:   107,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                102,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{"102108305275"},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "2S46Snsmc1IaISv",
-//		Name: "Clickadilla_Classic",
-//		ID:   108,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                102,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{"102108305275"},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "nimbHWu4CUtKUPP",
-//		Name: "Clickadilla_Classic",
-//		ID:   121,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                102,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{"102108305275"},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//	{
-//		Key:  "4uM4MVUnQtV8EPv",
-//		Name: "Clickadilla_Classic",
-//		ID:   122,
-//		DSP: []dsp.DSP{
-//			{
-//				ID:                102,
-//				Profit:            0.00,
-//				SourceIdBlacklist: []string{"102108305275"},
-//				CountryBlacklist:  []string{},
-//				CountryWhitelist:  []string{},
-//			},
-//		},
-//	},
-//}
